d6a80873e27dd9c9c69d77422cdab869df534878
000xzhou <xzhou92@gmail.com>
2024-05-11 17:11:40 -0400

    add route for create and find all

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,16 +7,56 @@ const express = require("express");
 
 const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
-const Company = require("../models/company");
+const Job = require("../models/job");
 
-const companyNewSchema = require("../schemas/companyNew.json");
-const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
 
 const router = new express.Router();
 
+/** POST / { job } =>  { job }
+ *
+ * job should be { id, title, salary, equity, company_handle }
+ *
+ * Returns { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: is admin
+ */
+
+router.post("/", ensureIsAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, jobNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const job = await Job.create(req.body);
+    return res.status(201).json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, company_handle }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - title
+ * - minSalary
+ * - equity (0>x>1)
+ *
+ * Authorization required: none
+ */
 router.get("/", async function (req, res, next) {
   try {
-    return res.json("hey");
+    // Retrieve query parameter from the URL
+    let { title, minSalary, equity } = req.query;
+
+    const jobs = await Job.findAll({
+      queryParams: { title, minSalary, equity },
+    });
+    return res.json({ jobs });
   } catch (err) {
     return next(err);
   }
